041bce81f61499debcc83cb595e86307fdfb989c
陳德元 <r06521608@ntu.edu.tw>
2021-07-21 03:11:55 +0800

    svg animate update

--- src/Projects/Day0003.js
+++ src/Projects/Day0003.js
@@ -47,6 +47,9 @@ const Pic5 = styled.svg`
     stroke: black;
     stroke-width: 2px;
     fill: none;
+    * {
+        transform-box: fill-box;
+    }
     & circle {
         fill: red;
         stroke: none;
@@ -57,6 +60,28 @@ const Pic5 = styled.svg`
         font-family: Futura;
         font-size: 10px;
     }
+    .frame_outer,.frame_inner,.neck,.bottom{
+        transform: scale(0);
+        transform-origin: bottom center;
+        transition-duration: 0.5s;
+        transition-delay: 0.5s;
+    }
+    .screen_content{
+        opacity: 0;
+        transition-duration: 0.5s;
+        transition-delay: 0s;
+    }
+    &:hover {
+        
+        .frame_outer,.frame_inner,.neck,.bottom {
+            transform: scale(1);
+            transition-delay: 0s;
+        }
+        .screen_content{
+            opacity: 1;
+            transition-delay: 0.5s;
+        }
+    }
 `
 
 function Day0003() {
@@ -66,40 +91,75 @@ function Day0003() {
             <Pic1 viewBox="-50 -50 100 100">
                 <line x1="-20" y1="0" x2="20" y2="0" />
                 <line x1="0" y1="-20" x2="0" y2="20" />
-                <circle cx="0" cy="0" r="10" />
+                <circle cx="0" cy="0" r="10">
+                    <animate attributeName="r" dur="2s" values="10;20;10" repeatCount="indefinite" />
+                </circle>
             </Pic1>
             <Pic2 viewBox="-50 -50 100 100">
                 <circle cx="0" cy="0" r="10" />
-                <line x1="-20" y1="0" x2="20" y2="0" />
-                <line x1="0" y1="-20" x2="0" y2="20" />
-                <rect x="-20" y="-20" width="40" height="40" />
+                <g>
+                    <line x1="-20" y1="0" x2="20" y2="0" />
+                    <line x1="0" y1="-20" x2="0" y2="20" />
+                    <rect x="-20" y="-20" width="40" height="40" />
+                    <animateTransform attributeName="transform" dur="2s" type="rotate" values="0;360" repeatCount="indefinite" />
+                </g>
+                
             </Pic2>
             <Pic3 viewBox="0 0 200 200">
-                <circle cx="0" cy="50" r="3" />
-                <circle cx="50" cy="80" r="3" />
-                <circle cx="100" cy="60" r="3" />
-                <circle cx="150" cy="20" r="3" />
-                <circle cx="200" cy="40" r="3" />
-                <polyline points="0,50 50,80 100,60 150,20 200,40" />
+                <circle cx="0" cy="50" r="3">
+                    <animate attributeName="cy" dur="2s" values="50;40;50" repeatCount="indefinite"/>
+                </circle>
+                <circle cx="50" cy="80" r="3">
+                    <animate attributeName="cy" dur="2s" values="80;100;80" repeatCount="indefinite"/>
+                </circle>
+                <circle cx="100" cy="60" r="3">
+                    <animate attributeName="cy" dur="2s" values="60;30;60" repeatCount="indefinite"/>
+                </circle>
+                <circle cx="150" cy="20" r="3">
+                    <animate attributeName="cy" dur="2s" values="20;50;20" repeatCount="indefinite"/>
+                </circle>
+                <circle cx="200" cy="40" r="3">
+                    <animate attributeName="cy" dur="2s" values="40;10;40" repeatCount="indefinite"/>
+                </circle>
+                <polyline points="0,50 50,80 100,60 150,20 200,40">
+                    <animate attributeName="points" dur="2s" repeatCount="indefinite" values="0,50 50,80 100,60 150,20 200,40;0,40 50,100 100,30 150,50 200,10;0,50 50,80 100,60 150,20 200,40" />
+                </polyline>
+                
             </Pic3>
             <Pic4 viewBox="0 0 200 200">
-                <rect x="0" y="0" width="20" height="100" />
-                <rect x="30" y="0" width="20" height="120" />
-                <rect x="60" y="0" width="20" height="140" />
-                <rect x="90" y="0" width="20" height="160" />
-                <rect x="120" y="0" width="20" height="180" />
-                <rect x="150" y="0" width="20" height="150" />
-                <rect x="180" y="0" width="20" height="120" />
+                <rect x="0" y="0" width="20" height="100" >
+                    <animate attributeName="height" begin="-0.3" dur="2s" values="100;200;100" repeatCount="indefinite" />
+                </rect>
+                <rect x="30" y="0" width="20" height="120" >
+                    <animate attributeName="height" begin="-0.6" dur="2s" values="100;200;100" repeatCount="indefinite" />
+                </rect>
+                <rect x="60" y="0" width="20" height="140" >
+                    <animate attributeName="height" begin="-0.9" dur="2s" values="100;200;100" repeatCount="indefinite" />
+                </rect>
+                <rect x="90" y="0" width="20" height="160" >
+                    <animate attributeName="height" begin="-1.2" dur="2s" values="100;200;100" repeatCount="indefinite" />
+                </rect>
+                <rect x="120" y="0" width="20" height="180" >
+                    <animate attributeName="height" begin="-1.5" dur="2s" values="100;200;100" repeatCount="indefinite" />
+                </rect>
+                <rect x="150" y="0" width="20" height="150" >
+                    <animate attributeName="height" begin="-1.8" dur="2s" values="100;200;100" repeatCount="indefinite" />
+                </rect>
+                <rect x="180" y="0" width="20" height="120" >
+                    <animate attributeName="height" begin="-2.1" dur="2s" values="100;200;100" repeatCount="indefinite" />
+                </rect>
             </Pic4>
             <Pic5 viewBox="-50 -40 100 100">
-                <rect x="-30" y="-20" width="60" height="40" />
-                <rect x="-25" y="-15" width="50" height="30" />
-                <rect x="-2" y="20" width="4" height="10" />
-                <rect x="-10" y="30" width="20" height="5" />
-                <line x1="-20" y1="0" x2="-8" y2="0" />
-                <line x1="-20" y1="5" x2="-5" y2="5" />
-                <circle cx="10" cy="0" r="7" />
-                <text x="-30" y="50" >My Computer</text>
+                <rect className="frame_outer" x="-30" y="-20" width="60" height="40" />
+                <rect className="frame_inner" x="-25" y="-15" width="50" height="30" />
+                <rect className="neck" x="-2" y="20" width="4" height="10" />
+                <rect className="bottom" x="-10" y="30" width="20" height="5" />
+                <g className="screen_content">
+                    <line x1="-20" y1="0" x2="-8" y2="0" />
+                    <line x1="-20" y1="5" x2="-5" y2="5" />
+                    <circle cx="10" cy="0" r="7" />
+                </g>
+                <text className="text" x="-30" y="50" >My Computer</text>
             </Pic5>
         </div>
     );
